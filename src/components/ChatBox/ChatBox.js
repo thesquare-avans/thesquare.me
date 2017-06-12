4da590206c230d848f076d3f546aa4ccbad2050b
Thomas van Aart <thomasvanaart@casema.nl>
2017-06-12 20:20:41 +0200

    ChatBox is mostly done. Can be integrated into the site when the api is finished. For now a simple echo server. See slack

--- src/components/ChatBox/ChatBox.js
+++ src/components/ChatBox/ChatBox.js
@@ -2,13 +2,9 @@ import * as React from "react";
 import {Divider, RaisedButton, TextField} from "material-ui";
 import {green500} from "material-ui/styles/colors";
 import io from 'socket.io-client'
-let chatRoom = "0"
+let chatRoom = "0";
 
-let messages = [
-  {username: "Thomas", message: "Hoi", uuid: "0"},
-  {username: "Thomas", message: "Hoi", uuid: "0"},
-  {username: "Thomas", message: "Hoi", uuid: "0"},
-];
+let messages = [];
 
 const styles = {
   button : {
@@ -21,6 +17,8 @@ const styles = {
   }
 };
 
+let socket = io('ws://localhost:3000');
+
 const ChatItem = (props) => (
   <li className={props.className}>
     <span className="username">{props.username}</span>: {props.text}
@@ -29,20 +27,41 @@ const ChatItem = (props) => (
 
 class ChatBox extends React.Component {
 
+  generateUUID () { // Public Domain/MIT
+    let d = new Date().getTime();
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+      d += performance.now(); //use high-precision timer if available
+    }
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      let r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+  }
+
+  handleSubmit(e) {
+    let data = {username: "Thomas", message: this.state.chat_text, person: "me", uuid: this.generateUUID()};
+    socket.emit("client:new_msg", data);
+    this.setState((prevState) => { prevState.messages.push(data) });
+    this.setState({chat_text: ""})
+  }
+
+  handleTextFieldChange(e) {
+    this.setState({chat_text: e.target.value})
+  }
+
   constructor(props) {
     super(props);
 
     this.state = {
+      chat_text: "",
       messages: messages
     }
   }
 
   componentDidMount() {
-    let socket = io('ws://localhost:3000');
-
     socket.on('server:new_msg', data => {
-      messages.push(data);
-      this.setState.messages = messages;
+      this.setState((prevState) => { prevState.messages.push(data) })
     });
   }
 
@@ -56,28 +75,27 @@ class ChatBox extends React.Component {
               <ul className="chatBox" style={{ padding: 0}}>
                 {this.state.messages.map(msg => {
                   return(
-                    <ChatItem className="me" text={msg.message} username={msg.username}/>
+                    <ChatItem className={msg.person} text={msg.message} username={msg.username} key={msg.uuid}/>
                   )
                 })}
               </ul>
               <Divider />
               <div className="bottom">
-                <TextField
-                  floatingLabelText="Chat"
-                  className="input-chat"
-                  style={{width: "70%"}}
-                  floatingLabelFocusStyle={styles.button.floatingLabelFocusStyle}
-                  underlineFocusStyle={styles.button.underlineStyle} />
-                <RaisedButton label="Send" style={{width: "10%", marginLeft: "20px"}}/>
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                  <TextField
+                    floatingLabelText="Chat"
+                    style={{width: "70%"}}
+                    value={this.state.chat_text}
+                    onChange={this.handleTextFieldChange.bind(this)}
+                    floatingLabelFocusStyle={styles.button.floatingLabelFocusStyle}
+                    underlineFocusStyle={styles.button.underlineStyle} />
+                  <RaisedButton label="Send" style={{width: "10%", marginLeft: "20px"}} onTouchTap={this.handleSubmit.bind(this)}/>
+                </form>
               </div>
             </div>
           </div>
-          <div className="row">
-
-          </div>
         </div>
       </div>
-
     )
   }
 }
